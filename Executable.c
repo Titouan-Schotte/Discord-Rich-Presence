cec28d784638bef7ffa4737828d9fd5357c30ae1
Titouan-Schotte <73279480+Titouan-Schotte@users.noreply.github.com>
2021-05-16 23:35:33 +0200

    Update Executable.c

--- Executable.c
+++ Executable.c
@@ -5,9 +5,8 @@ int main() {
 
     //Voir la config dans pour configurer votre rich presence
     printf("Play Rich Presence !\n");
-    printf("Créé par Titoune#1870");
+    printf("Créé par Titouan Schotte");
     //Simple executable du fichier config.js pour faire de sorte qu'il y ait un fichier .exe qui se créé à chaque nouveau run ...
     system("node ../config.js");
-    printf("SHUTDOWN ...");
     return 0;
 }
